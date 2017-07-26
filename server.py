8c73aa9b1d73dd9606bc7910ec8ce78ca2e1fbac
Imaduddin A Majid <imad@accelbyte.net>
2017-07-27 06:28:05 +0700

    Add POST method for /employees route

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
@@ -13,6 +14,30 @@ class Employees(Resource):
         conn = db_connect.connect() # connect to database
         query = conn.execute("select * from employees") # This line performs query and returns json result
         return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+    def post(self):
+        conn = db_connect.connect()
+        print(request.json)
+        LastName = request.json['LastName']
+        FirstName = request.json['FirstName']
+        Title = request.json['Title']
+        ReportsTo = request.json['ReportsTo']
+        BirthDate = request.json['BirthDate']
+        HireDate = request.json['HireDate']
+        Address = request.json['Address']
+        City = request.json['City']
+        State = request.json['State']
+        Country = request.json['Country']
+        PostalCode = request.json['PostalCode']
+        Phone = request.json['Phone']
+        Fax = request.json['Fax']
+        Email = request.json['Email']
+        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
+                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
+                             '{13}')".format(LastName,FirstName,Title,
+                             ReportsTo, BirthDate, HireDate, Address,
+                             City, State, Country, PostalCode, Phone, Fax,
+                             Email))
+        return {'status':'success'}
 
 class Tracks(Resource):
     def get(self):
@@ -27,7 +52,7 @@ class Employees_Name(Resource):
         query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
-        
+
 
 api.add_resource(Employees, '/employees') # Route_1
 api.add_resource(Tracks, '/tracks') # Route_2
