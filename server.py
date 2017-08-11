b2cd38ed63b057b5208de45f8a5049c8ee2e35d0
Sagar Chand Agarwal <scagarwal@kde.org>
2017-08-11 14:22:09 +0100

    Imports removed

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask.ext.jsonpify import jsonify
 
 db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
