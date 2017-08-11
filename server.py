e2c2402c2a115287fa918688f0cf199134196bf3
Sagar Chand Agarwal <scagarwal@kde.org>
2017-08-11 14:17:28 +0100

    Basic PEP 8 Changes

--- server.py
+++ server.py
@@ -9,11 +9,13 @@ db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
+
 class Employees(Resource):
     def get(self):
         conn = db_connect.connect() # connect to database
         query = conn.execute("select * from employees") # This line performs query and returns json result
         return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+    
     def post(self):
         conn = db_connect.connect()
         print(request.json)
@@ -39,6 +41,7 @@ class Employees(Resource):
                              Email))
         return {'status':'success'}
 
+    
 class Tracks(Resource):
     def get(self):
         conn = db_connect.connect()
@@ -46,6 +49,7 @@ class Tracks(Resource):
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
+    
 class Employees_Name(Resource):
     def get(self, employee_id):
         conn = db_connect.connect()
@@ -60,4 +64,4 @@ api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 
 
 if __name__ == '__main__':
-     app.run(port='5002')
+     app.run()
